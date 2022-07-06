e0dfdde11a451ed4642bac45fb3d5fd7ba0babf8
Prabhu Selvaraj <Prabhu.Selvaraj@XEBIAINDIA.LOCAL>
2022-07-06 22:03:59 +0530

    POCs uploaded

---
+++ AWS POCs/POCs/POC19 AWS SAM Demo/app.py
@@ -0,0 +1,22 @@
+import json
+
+import requests
+
+
+def lambda_handler(event, context):
+    
+    try:
+        ip= requests.get("http://checkip.amazonaws.com/")
+    except requests.RequestException as e:
+        print (e)
+        raise e
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world Prabhu",
+                # "location": ip.text.replace("\n", "")
+            }
+        ),
+    }
