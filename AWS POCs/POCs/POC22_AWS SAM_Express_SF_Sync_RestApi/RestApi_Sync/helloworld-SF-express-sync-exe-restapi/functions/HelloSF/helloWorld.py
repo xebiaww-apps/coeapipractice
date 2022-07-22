95073426c9bb55f8a079106d13b907593d1ad0f8
Prabhuselvaraj1234 <Prabhu19green@gmail.com>
2022-07-22 08:40:04 +0530

    added

---
+++ AWS POCs/POCs/POC22_AWS SAM_Express_SF_Sync_RestApi/RestApi_Sync/helloworld-SF-express-sync-exe-restapi/functions/HelloSF/helloWorld.py
@@ -0,0 +1,23 @@
+import json
+
+import requests
+
+
+def lambda_handler(event, context):
+    
+    try:
+        ip= requests.get("http://checkip.amazonaws.com/")
+        print ("e")
+    except requests.RequestException as e:
+        print (e)
+        raise e
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world from lambda Prabhu123",
+                "location": ip.text.replace("\n", "")
+            }
+        ),
+    }
